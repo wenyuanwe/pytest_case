c55ad511ad76dc97315c47aeb80664fb30d0a908
yuanwe <1486685095@qq.com>
2022-03-10 15:21:31 +0800

    修改main文件增加函数

--- py_test/main.py
+++ py_test/main.py
@@ -17,7 +17,10 @@ def che001():
     print('第二次运行测试代码')
 
 def che002():
-    print('第三次运行测试代码')    
+    print('第三次运行测试代码') 
+def che003():
+    print('第四次运行测试代码') 
+    
 if __name__ == '__main__':
     che()
 
